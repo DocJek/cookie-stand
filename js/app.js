6a5db0d290bf8f0fb75907ef1e2be643277e9282
DocJek <bassroscoe@gmail.com>
2017-06-12 16:56:00 -0700

    Got object methods working in first and Pike

--- js/app.js
+++ js/app.js
@@ -5,16 +5,22 @@ var firstAndPike = {
   minCust: 23,
   maxCust: 65,
   avgCookieSales: 6.3,
+  custAndCookiesPerHour: [],
   getCustPerHour: function() {
     return randNum(this.minCust, this.maxCust);
   },
-  getCookiePerHour: function() {
-    return Math.floor(this.getCustPerHour * this.avgCookieSales);
+  getCookiesPerHour: function() {
+    return Math.floor(this.getCustPerHour() * this.avgCookieSales);
   }
 };
 
 function randNum(min, max) {
-  return Math.floor(Math.random() * ((max - min + 1) + min));
+  return Math.floor(Math.random() * (max - min + 1)) + min;
 }
 
-console.log(firstAndPike.getCustPerHour);
+for (var i = 0; i < 15; i++) {
+  var cookie = firstAndPike.getCookiesPerHour();
+  firstAndPike.custAndCookiesPerHour.push(cookie[i]);
+}
+
+console.log(firstAndPike.custAndCookiesPerHour);
