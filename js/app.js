3cff4ef33dbab4f87a5f865d9a792adad5a4c028
DocJek <bassroscoe@gmail.com>
2017-06-12 18:26:55 -0700

    Finished first store object, prints info to html doc

--- js/app.js
+++ js/app.js
@@ -5,7 +5,6 @@ var firstAndPike = {
   minCust: 23,
   maxCust: 65,
   avgCookieSales: 6.3,
-  custAndCookiesPerHour: [],
   getCustPerHour: function() {
     return randNum(this.minCust, this.maxCust);
   },
@@ -18,9 +17,40 @@ function randNum(min, max) {
   return Math.floor(Math.random() * (max - min + 1)) + min;
 }
 
-for (var i = 0; i < 15; i++) {
+var custAndCookiesPerHour = [];
+var total = 0;
+
+for (var i = 0; i < 16; i++) {
   var cookie = firstAndPike.getCookiesPerHour();
-  firstAndPike.custAndCookiesPerHour.push(cookie[i]);
+  custAndCookiesPerHour.push(cookie)[i];
+  total += custAndCookiesPerHour[i];
 }
 
-console.log(firstAndPike.custAndCookiesPerHour);
+var parentEl = document.getElementById('firstPike');
+
+var article = document.createElement('article');
+parentEl.appendChild(article);
+
+var h2 = document.createElement('h2');
+article.appendChild(h2);
+h2.textContent = firstAndPike.name;
+
+var ul = document.createElement('ul');
+article.appendChild(ul);
+
+for (var x = 0; x < custAndCookiesPerHour.length; x++) {
+  var li = document.createElement('li');
+  li.textContent = custAndCookiesPerHour[x];
+  ul.appendChild(li);
+  if (x < 6) {
+    li.textContent = x + 6 + 'am: ' + custAndCookiesPerHour[x] + ' cookies';
+  } else if (x > 6) {
+    li.textContent = x - 6 + 'pm: ' + custAndCookiesPerHour[x] + ' cookies';
+  } else {
+    li.textContent = x + 6 + 'pm: ' + custAndCookiesPerHour[x] + ' cookies';
+  }
+}
+
+li = document.createElement('li');
+li.textContent = 'Total: ' + total;
+ul.appendChild(li);
